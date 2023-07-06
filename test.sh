9612280a2e14ac8427c9ed565c732507b9f49e9c
asapizhak <72894081+asapizhak@users.noreply.github.com>
2023-07-06 13:49:01 +0300

    added stub file for testing various things

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+###############################################################################
+### File for testing various things. No other purpose.
+### Committed for easier usage.
+
+. lib/lib_fs.sh
+. lib/lib_number_fn.sh
+. lib/lib_string_fn.sh
+
+set -u
+# set -e
+# set -x
