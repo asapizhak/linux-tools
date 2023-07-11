e3a53366c98d466a72523477fe24463c248bd2f0
asapizhak <72894081+asapizhak@users.noreply.github.com>
2023-07-11 13:47:50 +0300

    lib_ui added uiListWithSelection

--- lib/lib_ui.sh
+++ lib/lib_ui.sh
@@ -9,15 +9,67 @@ function uiDisplayList {
     declare -ri selection_idx=${2:--1}
     declare -r title=${3:-}
 
-    [[ -n $title ]] && echo2 "VVV $title VVV";
+    echo2
+    [[ -n $title ]] && echo2 "VVV $title VVV"
 
     for idx in "${!list[@]}"; do
-        if [[ $idx -ne -1 && $idx -eq $selection_idx ]]; then
+        if [[ $idx -ge 0 && $idx -eq $selection_idx ]]; then
             tput rev
             printf2 "\e[1m%s\e[0m\n" "${list[$idx]}"
             tput sgr0
         else
             printf2 "%s\n" "${list[$idx]}"
         fi
+    done
+}
+
+# uiListWithSelection selected_index_ref array_ref selection_index? title?
+function uiListWithSelection {
+    declare -n selected_idx_out=$1
+    declare -rn list=$2
+    declare -i selection_idx=${3:-0}
+    declare title=${4:-}
+
+    [[ -n $title ]] && title="$title, q to quit" || title="q to quit"
+
+    # man tput, man terminfo, https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x405.html
+    tput sc # save cursor pos before list
+    uiDisplayList files $selection_idx "$title"
+
+    declare input
+    while true; do
+        IFS= read -rsN1 input
+        [[ $input = $'\e' ]] && IFS= read -rsN2 input
+
+        case $input in
+        '[A') # arrow up
+            selection_idx=$((selection_idx-1))
+            ;;
+        '[B') # arrow down
+            ((selection_idx += 1))
+            ;;
+        $'\n') # Enter
+            # shellcheck disable=SC2034
+            selected_idx_out=$selection_idx
+            return 0
+            ;;
+        "q")
+            echo2 Quit requested
+            # shellcheck disable=SC2034
+            selected_idx_out=
+            return 1
+            ;;
+        *)
+            echo2 "Unknown key: $input"
+            ;;
+        esac
+
+        ((selection_idx < 0)) && selection_idx=0
+        ((selection_idx >= ${#list[@]})) && ((selection_idx = ${#list[@]} - 1))
+
+        tput rc # restore cursor pos
+        tput ed # clear to the end
+
+        uiDisplayList files $selection_idx "$title"
     done
 }
