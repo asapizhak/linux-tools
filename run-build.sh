6307a2c5db0be5d94a115a838dee06ad57a525cb
asapizhak <72894081+asapizhak@users.noreply.github.com>
2023-07-15 15:22:24 +0300

    run-build: added a couple of echos

--- run-build.sh
+++ run-build.sh
@@ -6,19 +6,20 @@ cd "$(dirname "${BASH_SOURCE[0]:-$0}")" || { echo >&2 "Failed to cd to script's 
 
 DIR='./build'
 
-rm -r "$DIR"
+rm -r "$DIR" && echo >&2 "Build: Removed previous directory $DIR"
 mkdir "$DIR"
 
 # lib
 LIB="lib"
 mkdir "$DIR/$LIB"
 find "./$LIB" -type f -name "*.sh" | while read -r file; do
-    cp "$file" "$DIR/$LIB/"
+    cp "$file" "$DIR/$LIB/" && echo >&2 "Build: $file copied"
 done
+echo >&2 "Build: Lib dir finished"
 
 # scripts
 find . -maxdepth 1 -type f ! -name "run-*.sh" ! -name "test.sh" -name "*.sh" | while read -r file; do
-    cp "$file" "$DIR/"
+    cp "$file" "$DIR/" && echo >&2 "Build: $file copied"
 done
-
+echo >&2 "Build: Done."
 cd "$PREV_DIR"
