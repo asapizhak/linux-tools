f97879d2ffc83a1fe1cf35dbe5cd58995f89d97f
asapizhak <72894081+asapizhak@users.noreply.github.com>
2023-07-20 22:06:21 +0300

    pkg lib: added getting command package
    and getting package version

--- lib/lib_packages.sh
+++ lib/lib_packages.sh
@@ -4,6 +4,31 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$DIR/lib_core.sh"
 
 
+# returns package name using command name or full executable path
+#    cmd - name or path
+#    out_pkg_name
+function pkgGetCommandPackage {
+    coreEnsureCommands dpkg-query
+    declare -r cmd="$1"
+    declare -n out_pkg_name=$2
+
+    declare cmd_full=
+    if [[ $cmd == /* ]]; then cmd_full="$cmd"; else cmd_full=$(which "$cmd"); fi
+
+    # shellcheck disable=SC2034
+    out_pkg_name="$(dpkg-query -S "$cmd_full" | cut -d ':' -f 1)"
+}
+
+#    pkg - package name
+#    out_version
+function pkgGetPackageVersion {
+    declare -r pkg="$1"
+    declare -n out_version=$2
+
+    # shellcheck disable=SC2034
+    out_version="$(dpkg-query -f '${Version}' -W "$pkg")"
+}
+
 # Compare version numbers, where op is a binary operator. There are two groups of operators,
 #  which differ in how they treat  an  empty ver1  or ver2.
 #  These treat an empty version as earlier than any version: lt le eq ne ge gt.
